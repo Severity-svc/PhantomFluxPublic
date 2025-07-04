a27a9a98c52530c81c7f9b57a457a156e7bbb46c
Severity <system0464@gmail.com>
2025-07-04 22:33:27 +0300

    added getdirectory()

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,28 @@
+if not getgenv().GetDirectory then
+	getgenv().GetDirectory = function(RunMode, FileName, URL)
+		if RunMode == "Developer Mode" then
+			local DirPath = "C:\\Users\\sentr\\OneDrive\\Documents\\GitHub\\PhantomFlux\\Profiles\\GrowAGarden\\"
+			local FilePath = DirPath .. FileName
 
+			if isfile(FilePath) then
+				local Source = readfile(FilePath)
+
+				if Source then
+					return loadstring(Source)()
+				else
+					error("[Handler]: Failed to read file: " .. FilePath)
+				end
+			else
+				error("[Handler]: File not found: " .. FilePath)
+			end
+		elseif RunMode == "Public" then
+			if URL then
+				return loadstring(game:HttpGet(URL))()
+			else
+				warn("[GetDirectory]: No url found")
+			end
+		else
+			error("[Handler]: Invalid RunMode")
+		end
+	end
+end

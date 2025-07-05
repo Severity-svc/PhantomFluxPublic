32284990fbd1859fe537f334dc64c0c3e0ef4eea
Severity <system0464@gmail.com>
2025-07-05 10:09:47 +0300

    Restore Import

--- GlobalUtility.lua
+++ GlobalUtility.lua
@@ -4,4 +4,42 @@ local Collection = {
 	},
 }
 
+function Collection:RestoreImport()
+	getgenv().Import = function(RunMode, FileName)
+		if RunMode == "Developer" then
+			local DirPath = "C:/Users/sentr/OneDrive/Documents/GitHub/PhantomFlux/"
+			local FilePath = DirPath .. FileName
+
+			if isfile(FilePath) then
+				local Source = readfile(FilePath)
+				if Source then
+					return loadstring(Source)()
+				else
+					error("[Import]: Failed to read file: " .. FilePath)
+				end
+			else
+				error("[Import]: File not found: " .. FilePath)
+			end
+		elseif RunMode == "Web" then
+			local BaseUrl = "https://raw.githubusercontent.com/Severity-svc/PhantomFluxPublic/main/"
+			local FilePath = BaseUrl .. FileName
+
+			local Success, Response = pcall(function()
+				return game:HttpGet(FilePath)
+			end)
+
+			if Success and Response then
+				if Response:find("<html>") then
+					error("[Import]: Invalid response content: " .. FilePath)
+				end
+				return loadstring(Response)()
+			else
+				error("[Import]: Failed to fetch file from URL: " .. FilePath)
+			end
+		else
+			error("[Import]: Invalid RunMode")
+		end
+	end
+end
+
 return Collection
